6a6e8dd796d74d7d5836ac0aed701044cbb3edde
Liottt <80908141+Liottt@users.noreply.github.com>
2021-03-22 22:55:22 +0600

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,14 @@
+import telegrambot
+
+bot = telebot.TeleBot("1726681314:AAFhj0l-djAxu1bd7HxafgJNBDI5FjkoM-M")
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Привет я пока могу только повторять за тобой! Напиши сообщение:")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+	bot.reply_to(message, message.text)
+
+bot.polling()
